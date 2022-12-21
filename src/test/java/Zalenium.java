957faa98b0db416adb688fd4ba57b233ff7a7834
Almog Noach <almog81@gmail.com>
2022-12-21 11:32:17 +0200

    Update Zelenium Test Firefox

--- src/test/java/Zalenium.java
+++ src/test/java/Zalenium.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
@@ -13,32 +15,31 @@ import java.net.URL;
 
 public class Zalenium {
 
-    static WebDriver driver;
+    public static WebDriver driver;
+    public static DesiredCapabilities cap;
 
     @BeforeClass
     @Parameters({"browserName"})
     public static void startSession(String browser) throws MalformedURLException {
         if (browser.equalsIgnoreCase("Chrome")) {
-            DesiredCapabilities cap = DesiredCapabilities.chrome();
+            cap = DesiredCapabilities.chrome();
             driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
         }else if (browser.equalsIgnoreCase("firefox")) {
-            DesiredCapabilities cap = DesiredCapabilities.firefox();
-
+            cap = DesiredCapabilities.firefox();
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
         }else
             throw new RuntimeException("Wrong Browser Name");
         driver.manage().window().maximize();
-        driver.get("https://www.whatismybrowser.com/");
     }
-
-    @AfterClass
-    public static void endSession(){
-        driver.quit();
-    }
-
     @Test
-    public static void Test01_BrowserCheck(){
+    public static void Test01_browserCheck(){
+        driver.get("https://www.whatismybrowser.com/");
         WebElement Text = driver.findElement(By.xpath("//*[@id=\"primary-browser-detection-backend\"]/div[1]/a"));
         String Result = Text.getText();
         System.out.println(Result);
+    }
+    @AfterClass
+    public static void endSession(){
+        driver.quit();
     }
 }
